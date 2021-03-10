26279e27cfa1fd589cf439faf92036d91854f2ef
Alejo Jimenez <ajimenez.rengifo@gmail.com>
2021-03-10 14:13:05 -0300

    Upload Dependencies Installed

---
+++ medi-dynamo/src/component/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <>
+            <nav class="navbar navbar-light bg-light">
+                <a class="navbar-brand" href="#">
+                    <img src="/docs/4.6/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+                    Bootstrap
+                </a>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar;
